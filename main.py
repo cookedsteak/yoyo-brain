cb1851eb698478c172e4a6000c5a58d2cbc841e0
郭睿立 <cookedsteak708@gmail.com>
2020-01-13 10:51:07 +0800

    fix

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from configparser import ConfigParser
 import argparse
 import importlib
 import string
-from multiprocessing import Process
+# from multiprocessing import Process
 from utils import AliBucket, RedisCache, MQNormal
 
 
-def start_consumer(cfg, args, model):
+def start_consumer(cfg, args):
+    model = args['model']
     # import wisdom 下的类名(识别方法类)
     mod = importlib.import_module('models.' + model + '.' + model)
     # 获取类名
@@ -18,7 +19,9 @@ def start_consumer(cfg, args, model):
     ins.set_cacher(RedisCache(args['config']))
 
     # 队列
-    args['queue'] = model
+    if args['queue'] == 'default':
+        args['queue'] = model
+
     if cfg.getint('app', 'debug') == 1:
         mq = MQNormal(cfg).start_mq_debug()
     else:
@@ -43,24 +46,17 @@ if __name__ == '__main__':
     # 你想要监听的队列
     ap.add_argument("-q", "--queue", default="default", help="the queue you wanna listen")
     # 你想要指定使用的识别模型
-    ap.add_argument("-m", "--model", default="default", help="select the model")
+    ap.add_argument("-m", "--model", required=True, help="select a model")
     # 配置文件位置
     ap.add_argument("-c", "--config", default="config.ini", help="config path")
+    # 命令行配置
     a = vars(ap.parse_args())
+    # 环境变量
     c = ConfigParser()
     c.read(a['config'])
     models = c.get('app', 'models').split(",")
 
-    if a['model'] == 'default':
-        for model in models:
-            p = Process(target=start_consumer, args=(c, a, model))
-            p.start()
-            # p.join()
-
-    else:
-        if a['queue'] == 'default':
-            start_consumer(c, a, a['queue'])
-
+    start_consumer(c, a)
 
 
 
